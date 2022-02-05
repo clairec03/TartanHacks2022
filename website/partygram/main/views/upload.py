83f16be61c572e33007df60949d3263789998fc7
czhu1217 <cindyzhu0830@gmail.com>
2022-02-04 22:53:40 -0500

    added file upload

--- website/partygram/main/views/upload.py
+++ website/partygram/main/views/upload.py
@@ -4,7 +4,7 @@ from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 import face_recognition
-from .models import User, Encoding, Image
+from main.models import User, Encoding, Image
 
 
 def upload(request):
@@ -12,8 +12,8 @@ def upload(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        context['url'] = fs.url(name)
+        filename = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(filename)
     return render(request, 'upload.html', context)
 
 def image_processing(image_url):
--- website/partygram/partygram/urls.py
+++ website/partygram/partygram/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('', include('main.urls'))
-=======
+    # path('', include('main.urls'))
     path('upload', views.upload, name='upload'),
->>>>>>> 066605b2e676f616f492e31c6da353ddc6e446bd
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

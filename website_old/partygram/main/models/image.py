9ea3c0db0d5596dec319779d3f85744fbb111434
czhu1217 <cindyzhu0830@gmail.com>
2022-02-05 01:09:51 -0500

    got rid of website old

--- website_old/partygram/main/models/image.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from .user import User
-# Create your models here.
-
-class Image(models.Model):
-    img = models.ImageField(upload_to = 'uploads/pictures')
-    people = models.ManyToManyField(User)

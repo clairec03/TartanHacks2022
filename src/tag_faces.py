209daaa35796c4a129d28d8f1a477d4901b76d8a
Claire Chen <clairechen@claires-air.wifi.local.cmu.edu>
2022-02-04 19:44:36 -0500

    face tagging

--- src/tag_faces.py
+++ src/tag_faces.py
@@ -15,13 +15,21 @@ face_locations = face_recognition.face_locations(
 
 print(len(face_locations))
 
-for file in os.listdir(rootdir):
-    d = os.path.join(rootdir, file)
-    if os.path.isdir(d):
-        print(d)
+result = dict()
 
 for face_location in face_locations:
     top, right, bottom, left = face_location
     face_image = picture[top:bottom, left:right]
     pil_image = Image.fromarray(face_image)
     pil_image.show()
+    print(PICTURE)
+    for file in os.listdir(rootdir):
+        print(file)
+        d = os.path.join(rootdir, file)
+        known_image = face_recognition.load_image_file(f"{file}/init.jpg")
+        unknown_image = face_recognition.load_image_file(f"{PICTURE}")
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        if os.path.isdir(d) and result[0]:
+            print(f"Face matched! Image features {face_location} who is {file}")
--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 import face_recognition
-known_image = face_recognition.load_image_file("faces/tomcruise.jpg")
-unknown_image = face_recognition.load_image_file("faces/unknown.jpg")
+known_image = face_recognition.load_image_file("faces/tom_cruise/init.jpg")
+unknown_image = face_recognition.load_image_file("pictures/picture9.jpg")
 
 biden_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]

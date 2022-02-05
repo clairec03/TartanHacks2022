f8c9ab314174666b76c6535e7050d950fa5d555a
Claire Chen <clairechen@claires-air.wifi.local.cmu.edu>
2022-02-04 20:07:01 -0500

    edited face tagging

--- src/tag_faces.py
+++ src/tag_faces.py
@@ -16,20 +16,21 @@ face_locations = face_recognition.face_locations(
 print(len(face_locations))
 
 result = dict()
+# (image file name, face_location)
 
 for face_location in face_locations:
     top, right, bottom, left = face_location
-    face_image = picture[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
+    unknown_image = picture[top:bottom, left:right]
+    pil_image = Image.fromarray(unknown_image)
     pil_image.show()
     for file in os.listdir(rootdir):
-        print(file)
         if not file == ".DS_Store":
             d = os.path.join(rootdir, file)
             known_image = face_recognition.load_image_file(f"{ROOTDIR}{file}/init.jpg")
-            unknown_image = face_recognition.load_image_file(f"{PICTURE}")
             known_encoding = face_recognition.face_encodings(known_image)[0]
             unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-            results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+            results = face_recognition.compare_faces(known_encoding, unknown_encoding)
             if os.path.isdir(d) and results[0]:
                 print(f"Face matched! Image features {face_location} who is {file}")
+                # if (PICTURE, face_location) not in result:
+                result[(PICTURE, face_location)] = file

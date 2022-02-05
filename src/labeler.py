ad1211067362da013b51f00ef999f77792653ad2
Siyuan Daniel Chen <siyuanc2@andrew.cmu.edu>
2022-02-05 02:55:22 -0500

    refactored labeler.py

--- src/labeler.py
+++ src/labeler.py
@@ -3,27 +3,28 @@ from params import *
 from tagger import *
 
 
+def toDisplayName(user):
+    assert '_' in user
+    return ' '.join(name.capitalize() for name in user.split('_'))
+
+
 if __name__ == "__main__":
     picture = PICDIR + PICTURE
     tagger = Tagger(picture)
     users = tagger.getUsers()
-    print(users)
-    with Image.open(picture) as im:
+    with Image.open(picture) as image:
         for user in users:
             for location in tagger.getLocations(user):
-                y0, x1, y1, x0 = location
-                draw = ImageDraw.Draw(im, 'RGBA')
-                # (x0, y0, x1, y1)
-                draw.line([(x0, y0), (x1, y0)], fill=(93, 249, 7, 227)) # Upper horizontal line
-                draw.line([(x0, y1), (x1, y1)], fill=(93, 249, 7, 227)) # Lower horizontal line
-                draw.line([(x0, y0), (x0, y1)], fill=(93, 249, 7, 227)) # Left vertical line
-                draw.line([(x1, y0), (x1, y1)], fill=(93, 249, 7, 227)) # Right vertical line
-                display_name = ""
-                for string in user.split("_"):
-                    first_char = string[0].upper()
-                    rem_chars = string[1:]
-                    display_name += first_char
-                    display_name += rem_chars
-                    display_name += " "
-                draw.text((x0, y1 + 10), display_name, fill=(93, 249, 7, 227), anchor="mm") # Displays name
-        im.save(OUTDIR + PICTURE)
+                top, right, bottom, left = location
+                mid = left + (right - left) / 2
+                draw = ImageDraw.Draw(image, 'RGBA')
+                draw.polygon([(left, top), (left, bottom), (right, bottom), (right, top)],
+                        width=LINEWIDTH,
+                        outline=OUTLINECLR
+                        )
+                draw.text((mid, bottom), toDisplayName(user),
+                        anchor="ma",
+                        font=TEXTFONT,
+                        fill=TEXTCLR
+                        )
+        image.save(OUTDIR + PICTURE)
--- src/params.py
+++ src/params.py
@@ -1,6 +1,16 @@
+from PIL import ImageFont
+
+
 USRDIR = "users/"
 PICDIR = "pictures/"
 OUTDIR = "output/"
+FONTDIR = "fonts/"
+
+USRANON = "unknown_unknown"
+
+LINEWIDTH = 3
+OUTLINECLR = (0, 255, 0, 255)
+TEXTCLR = (0, 255, 0, 255)
+TEXTFONT = ImageFont.truetype(FONTDIR + "JetBrainsMono-Regular.ttf", 14)
 
 PICTURE = "picture5.jpg"
-USRANON = "unknown_unknown"

c6166e1fca5ea5d9435f25f0c2cd3f42cd5ee899
La-Shallot <richwang520@gmail.com>
2022-02-04 21:52:17 -0500

    uwu

--- website/partygram/main/models/user.py
+++ website/partygram/main/models/user.py
@@ -1,13 +1,17 @@
 from django.db import models
+
 import numpy
 
 # Create your models here.
-class CustomEncodingManager(BaseEncodingManager):
+class CustomEncodingManager(models.Manager):
     def create_encoding(self, encoding_obj):
         encoding_list = numpy.ndarray.tolist(encoding_obj)
         encoding = self.model(serialized_encoding = encoding_list)
         encoding.save()
         return encoding
+
+    # def get_encoding_nparray(self):
+
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
@@ -16,3 +20,7 @@ class User(models.Model):
 class Encoding(models.Model):
     serialized_encoding = models.JSONField()
     person = models.ForeignKey(User)
+    objects = CustomEncodingManager()
+
+    def get_numpy_array(self):
+        return numpy.array(self.serialized_encoding)
--- website/partygram/main/views/upload.py
+++ website/partygram/main/views/upload.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView, ListView, CreateView
 from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+import face_recognition
+from .models import User, Encoding, Image
 
 @login_required
 def upload(request):
@@ -13,3 +15,35 @@ def upload(request):
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
     return render(request, 'upload.html', context)
+
+def image_processing(image_url):
+    picture = face_recognition.load_image_file(image_url)
+    face_locations = face_recognition.face_locations(
+            picture,
+            number_of_times_to_upsample=1,
+            model="cnn"
+    )
+
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        face = picture[top:bottom, left:right]
+        encoding = face_recognition.face_encodings(face)
+        if len(encoding) == 0:
+            continue
+        encoding = encoding[0]
+
+        #  Image.fromarray(face).show()
+        users = User.objects.all()
+        
+        for user in users:
+            user_encodings = Encoding.objects.filter(reporter__first_name='John')
+            if len(user_encoding) == 0:
+                continue
+            user_encoding = user_encoding[0]
+            result = face_recognition.compare_faces([encoding], user_encoding)
+            if result[0]:
+                print(user)
+                Image.fromarray(face).show()
+                Image.fromarray(user_face).show()
+    
+

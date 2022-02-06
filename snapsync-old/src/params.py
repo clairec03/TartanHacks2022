6a95125887ff068be1808abc8181a11a2d8c6892
Siyuan Daniel Chen <siyuanc2@andrew.cmu.edu>
2022-02-06 06:37:38 -0500

    moved to snapsync-old

---
+++ snapsync-old/src/params.py
@@ -0,0 +1,12 @@
+from PIL import ImageFont
+
+ROOTDIR = "snapsync-old/"
+
+USRDIR = ROOTDIR + "users/"
+PICDIR = ROOTDIR + "pictures/"
+OUTDIR = ROOTDIR + "output/"
+FONTDIR = ROOTDIR + "fonts/"
+
+USRANON = "unknown_unknown"
+
+TEXTFONT = ImageFont.truetype(FONTDIR + "JetBrainsMono-Regular.ttf", 14)
